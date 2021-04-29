4c0eb5450dc2d2369e238ff020f5ecb309712545
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2021-04-29 14:52:46 +0200

    change quizListItem, add copy to clipboard btn

--- components/QuizList/QuizListItem/index.js
+++ components/QuizList/QuizListItem/index.js
@@ -1,17 +1,43 @@
 import styles from "./index.module.scss";
 
+import copy from "copy-to-clipboard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 import Button from "../../Button";
 
 const QuizListItem = ({ title, _id, deleteQuiz }) => {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+  const [quizLink, setQuizLink] = useState("");
+
+  useEffect(() => {
+    setQuizLink(`${window.location.origin}/Quiz/${_id}`);
+  }, []);
+
+  const handleCopyLink = () => {
+    copy(quizLink);
+
+    setIsTooltipVisible(true);
+
+    setTimeout(() => {
+      setIsTooltipVisible(false);
+    }, 2000);
+  };
+
   return (
     <div className={styles.container} key={_id}>
       <h3 className={styles.title}>{title}</h3>
+      <p className={styles.link}>{quizLink}</p>
+
+      {isTooltipVisible && (
+        <div className={styles.tooltip}>
+          <FontAwesomeIcon icon={faCheck} />
+        </div>
+      )}
 
       <div className={styles.settings}>
         <Link href={`/Quiz/${_id}`}>
@@ -21,8 +47,8 @@ const QuizListItem = ({ title, _id, deleteQuiz }) => {
             </Button>
           </a>
         </Link>
-        <Button size="small">
-          Get link <FontAwesomeIcon icon={faLink} />
+        <Button size="small" onClick={handleCopyLink}>
+          Copy link <FontAwesomeIcon icon={faLink} />
         </Button>
 
         <Button danger size="small" onClick={() => deleteQuiz(_id)}>
