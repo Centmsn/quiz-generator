a7babce7a95cd46e2e0bd1ab8be44206768d79f9
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2021-04-30 13:52:29 +0200

    fix components shadows, add settings info

--- components/Settings/index.js
+++ components/Settings/index.js
@@ -1,7 +1,20 @@
 import styles from "./index.module.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTools } from "@fortawesome/free-solid-svg-icons";
+
 const Settings = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Settings</h2>
+
+      <h3 className={styles.temporaryTitle}>Site under construction</h3>
+      <h3 className={styles.temporaryTitle}>Please be patient</h3>
+      <h4 className={styles.temporaryTitle}>
+        <FontAwesomeIcon icon={faTools} />
+      </h4>
+    </div>
+  );
 };
 
 export default Settings;
