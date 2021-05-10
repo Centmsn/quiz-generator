d878162cef95399cb00a4c0eea884f4afc14c386
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2021-05-10 15:47:22 +0200

    add error handling to deletequiz endpoint

--- pages/api/deletequiz/[id].js
+++ pages/api/deletequiz/[id].js
@@ -1,4 +1,5 @@
 import { getSession } from "next-auth/client";
+import mongoose from "mongoose";
 
 import { connectToDb } from "utils/connectToDb";
 import User from "models/user";
@@ -20,14 +21,28 @@ const handler = async (req, res) => {
     const currentUser = await User.findOne({ email: session.user.email });
     const currentQuiz = await Quiz.findById(req.query.id);
 
-    // todo add mongoose session
-    //! commit session if no error occured
-    // ! add error handling
-    await currentUser.quizes.pull(currentQuiz);
-    await currentUser.save();
-    await Quiz.deleteOne({ _id: currentQuiz });
+    if (!currentQuiz || !currentUser) {
+      res.status(500).json({
+        message: "500 internal server error",
+      });
+    }
 
-    res.json({ message: "OK" });
+    try {
+      const session = await mongoose.startSession();
+      session.startTransaction();
+
+      await currentUser.quizes.pull(currentQuiz);
+      await currentUser.save({ session });
+      await Quiz.deleteOne({ _id: currentQuiz }, { session });
+
+      await session.commitTransaction();
+    } catch (error) {
+      res.status(500).json({
+        message: "500 internal server error",
+      });
+    }
+
+    res.status(200).json({ message: "OK" });
   }
 };
 
