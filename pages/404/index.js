b52549ebebc4dcdb2862c5e0ce69e621571bfe7c
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2021-05-10 14:36:11 +0200

    add custom 404 page

---
+++ pages/404/index.js
@@ -0,0 +1,28 @@
+import styles from "./404.module.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSadTear } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
+import Button from "components/Button";
+
+const NotFound = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        Page not found <FontAwesomeIcon icon={faSadTear} />
+      </h1>
+
+      <h3 className={styles.subTitle}>
+        Button below will take You to the home page
+      </h3>
+      <Link href="/">
+        <a>
+          <Button size="small">Home</Button>
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
